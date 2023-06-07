b0235ea2c9004df850b94278355ed176545d00de
Paulo Victor <paulo.victor.ig@hotmail.com>
2023-06-07 09:27:31 -0300

    rotas criadas para navegacao entre as paginas e criação do componente menu para auxilio nessa navegacao

---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="routa in rotas">
+        <router-link :to="routa.path ? routa.path : '/'">
+          {{ routa.titulo }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    rotas: {
+      type: Array,
+      required: true,
+    }
+  }
+};
+
+</script>
+
+<style></style>
